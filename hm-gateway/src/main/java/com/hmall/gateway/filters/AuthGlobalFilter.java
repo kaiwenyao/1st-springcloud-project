01a63884292182a296372a8645505d96837cfe3f
Kaiwen Yao <kaiwen.yao.cn@outlook.com>
2025-12-08 12:32:59 +0000

    登录校验

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -0,0 +1,74 @@
+package com.hmall.gateway.filters;
+
+
+import cn.hutool.core.text.AntPathMatcher;
+import com.hmall.common.exception.UnauthorizedException;
+import com.hmall.gateway.config.AuthProperties;
+import com.hmall.gateway.util.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    private final AuthProperties authProperties;
+    private final JwtTool jwtTool;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 获取request
+        ServerHttpRequest request = exchange.getRequest();
+        // 判断是否拦截
+        if (isExclude(request.getPath().toString())) {
+            // 放行
+            return chain.filter(exchange);
+        }
+        // 获取token
+        String token = null;
+        List<String> headers = request.getHeaders().get("authorization");
+        if (headers != null && !headers.isEmpty()) {
+            token = headers.get(0);
+        }
+        // 校验并且解析token
+        Long userId = null;
+        try {
+            userId = jwtTool.parseToken(token);
+        } catch (UnauthorizedException e) {
+            // response status code 401
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        // TODO 传递用户信息
+        System.out.println("userid = " + userId);
+        return chain.filter(exchange);
+
+    }
+
+    private boolean isExclude(String path) {
+        for (String pathPattern : authProperties.getExcludePaths()) {
+            if (antPathMatcher.match(pathPattern, path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
